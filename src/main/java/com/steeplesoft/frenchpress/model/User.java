fd4fb56d3df4f39001cb76b22d405390cf5f08ea
Jason Lee <jason@steeplesoft.com>
2010-07-08 22:50:09 -0500

    Adding files to SCM

---
+++ src/main/java/com/steeplesoft/frenchpress/model/User.java
@@ -0,0 +1,152 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steeplesoft.frenchpress.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author jasonlee
+ */
+@Entity
+@Table(name = "user")
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+    @NamedQuery(name = "User.findById", query = "SELECT u FROM User u WHERE u.id = :id"),
+    @NamedQuery(name = "User.findByEmailAddress", query = "SELECT u FROM User u WHERE u.emailAddress = :emailAddress"),
+    @NamedQuery(name = "User.findByFirstName", query = "SELECT u FROM User u WHERE u.firstName = :firstName"),
+    @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName")
+})
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emailAddress", length = 255)
+    @Email
+    private String emailAddress;
+
+    @Column(name = "firstName", length = 255)
+    @NotEmpty
+    private String firstName;
+
+    @Column(name = "lastName", length = 255)
+    @NotEmpty
+    private String lastName;
+
+    @OneToMany(mappedBy = "memberId")
+    private Collection<Registration> registrationCollection;
+
+    @Column
+    private Boolean canPost;
+
+    @Column
+    private Boolean isAdmin;
+
+    public User() {
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getCanPost() {
+        return canPost;
+    }
+
+    public void setCanPost(Boolean canPost) {
+        this.canPost = canPost;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Collection<Registration> getRegistrationCollection() {
+        return registrationCollection;
+    }
+
+    public void setRegistrationCollection(Collection<Registration> registrationCollection) {
+        this.registrationCollection = registrationCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.steeplesoft.frenchpress.model.User[id=" + id + "]";
+    }
+}
---
+++ src/test/java/com/steeplesoft/frenchpress/test/MainServiceTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steeplesoft.frenchpress.test;
+
+import org.junit.Test;
+import com.steeplesoft.frenchpress.model.Sponsor;
+import com.steeplesoft.frenchpress.service.MainService;
+import javax.inject.Inject;
+import static org.junit.Assert.*;
+
+//@RunWith(Arquillian.class)
+public class MainServiceTest {
+    @Inject
+    MainService mainService;
+
+//    @Deployment
+//    public static Archive<?> createTestArchive() {
+//        return ShrinkWrap.create("test.jar", JavaArchive.class)
+//                .addClasses(MainService.class, MainServiceImpl.class, GroupMember.class,
+//                BlogEntry.class, Registration.class, Sponsor.class);
+//    }
+
+//    @Test
+    public void testgetRandomSponsor() {
+        Sponsor sponsor = mainService.getRandomSponsor();
+        assertNotNull(sponsor);
+    }
+    @Test
+    public void dummy() {
+        
+    }
+}
