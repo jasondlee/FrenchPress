dfb65582cb61a3f2a3bc47dbd49798f8f9c8b615
Jason Lee <jason@steeplesoft.com>
2011-01-13 00:11:23 -0600

    Version upgrades Add User.class to the test to satisfy JPA

--- src/test/java/com/steeplesoft/frenchpress/service/impl/PostServiceTest.java
+++ src/test/java/com/steeplesoft/frenchpress/service/impl/PostServiceTest.java
@@ -1,8 +1,9 @@
 package com.steeplesoft.frenchpress.service.impl;
 
 import org.jboss.shrinkwrap.api.ArchivePaths;
-import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import com.steeplesoft.frenchpress.model.Post;
+import com.steeplesoft.frenchpress.model.User;
 import com.steeplesoft.frenchpress.service.PostService;
 import com.steeplesoft.frenchpress.service.Transactional;
 import com.steeplesoft.frenchpress.service.TransactionalInterceptor;
@@ -13,7 +14,6 @@ import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import static org.junit.Assert.*;
 
@@ -26,7 +26,7 @@ public class PostServiceTest {
     @Deployment
     public static Archive<?> createDeployment() {
         return ShrinkWrap.create(WebArchive.class, "frenchpress.war")
-                .addClasses(Post.class, PostService.class, PostServiceImpl.class, TransactionalInterceptor.class, Transactional.class)
+                .addClasses(Post.class, User.class, PostService.class, PostServiceImpl.class, TransactionalInterceptor.class, Transactional.class)
                 .addManifestResource("test-persistence.xml", "persistence.xml")
                 .addWebResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
 //                .addWebResource("beans.xml");
