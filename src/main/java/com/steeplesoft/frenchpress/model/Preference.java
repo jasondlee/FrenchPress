89a6e33b2ed50f3fc988e80355854f2e81cf019f
Jason Lee <jason@steeplesoft.com>
2011-03-02 16:21:07 -0600

    Re-add some model and utility classes

---
+++ src/main/java/com/steeplesoft/frenchpress/model/Preference.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.steeplesoft.frenchpress.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author jasonlee
+ */
+@Entity
+@Table(name="preferences")
+public class Preference implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String name;
+    @Column
+    private String value;
+
+    public Preference() {
+
+    }
+
+    public Preference(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (name != null ? name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Preference)) {
+            return false;
+        }
+        Preference other = (Preference) object;
+        if ((this.name == null && other.name != null) || (this.name != null && !this.name.equals(other.name))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return getValue();
+    }
+}
