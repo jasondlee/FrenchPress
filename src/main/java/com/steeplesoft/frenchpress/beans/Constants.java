69be6a6d2278012fd42054610473d135674630a9
Jason Lee <jason@steeplesoft.com>
2012-02-17 17:01:34 -0600

    Move constants to util class Experimental entity interface (for paging code)

---
+++ src/main/java/com/steeplesoft/frenchpress/beans/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steeplesoft.frenchpress.beans;
+
+/**
+ *
+ * @author jdlee
+ */
+public class Constants {
+    public static final String VIEW_REDIRECT = "faces-redirect=true";
+    public static final String VIEW_ADMIN_POSTS_INDEX = "/admin/posts/index.xhtml?" + VIEW_REDIRECT;
+}
---
+++ src/main/java/com/steeplesoft/frenchpress/model/FpEntity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.steeplesoft.frenchpress.model;
+
+/**
+ *
+ * @author jdlee
+ */
+public interface FpEntity {
+    Long getId();
+    void setId(Long id);
+}
